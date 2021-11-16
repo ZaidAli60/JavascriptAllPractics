fbb4732cf2259915e6554970724ee65a8b5e0bda
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-11-16 05:03:20 -0800

    lower and upper case

--- New folder/app.js
+++ New folder/app.js
@@ -18,3 +18,24 @@
 //         alert('`City has not found')
 //     }
 // };
+
+
+
+var city = prompt("Enter Your city");
+var firstCharater = city.slice(0,1)
+firstCharater = firstCharater.toUpperCase()
+var otherCharater = city.slice(1)
+otherCharater = city.slice(1)
+
+var lowerUpper =firstCharater+otherCharater;
+console.log(lowerUpper);
+
+var arr = ['faisalabad', 'lahore', 'multan'];
+
+for (var i = 0; i < arr.length; i++){
+    if (arr[i] === city){
+        alert('City has found')
+    }else{
+        alert('`City has not found')
+    }
+};
