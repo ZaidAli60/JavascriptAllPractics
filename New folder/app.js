8544e8bb94c261a85f62bb46ee5f2fa04f8776eb
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-11-16 03:25:51 -0800

    for loop practics

---
+++ New folder/app.js
@@ -0,0 +1,20 @@
+// for(var a =1; a < 100; a =a+10){
+//     for(var b = a; b<a+10; b++){
+//         document.write(b + " " );
+//     }
+//     document.write("<br>");
+// }
+
+
+// var city = prompt("Enter Your city");
+// city = city.toLowerCase();
+
+// var arr = ['faisalabad', 'lahore', 'multan'];
+
+// for (var i = 0; i < arr.length; i++){
+//     if (arr[i] === city){
+//         alert('City has found')
+//     }else{
+//         alert('`City has not found')
+//     }
+// };
