91f7b0f0c6246ade1c6d4b2f3eb146e87ad1bbf7
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-12-17 03:36:24 -0800

    declear the var

---
+++ Todo localstorage/app.js
@@ -0,0 +1,12 @@
+const form = document.querySelector(".from-control")
+const inputControl = document.getElementById("input");
+const todoContainer = document.querySelector(".todo-container")
+const lists = document.querySelector(".list-container")
+const clearBtn = document.querySelector(".clear-btn")
+
+// edit options
+
+let editElement ;
+let editFlag = false;
+let editID = "";
+
