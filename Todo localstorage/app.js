751f0c993bbd002e6af9e1c36ecd4f917ddc772a
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-12-17 08:25:46 -0800

    show the value in js

--- Todo localstorage/app.js
+++ Todo localstorage/app.js
@@ -21,14 +21,35 @@ function addItem(e){
     const id = new Date().getTime().toString();
    
     if (value && !editFlag) {
-        console.log("add item to list")
-    } else if (value && editFlag){
+        const element = document.createElement("article")
+        element.classList.add("todo-item")
+    //    add id
+        const arrt = document.createAttribute("date-id")
+            arrt.value = id;
+           element.setAttributeNode(arrt)
+           element.innerHTML =`<p class="title">${value}</p>
+           <div class="btn-container">
+               <button class="edit-btn"><i class="bi bi-pencil-square"></i></button>
+               <button class="delete-btn"><i class="bi bi-archive-fill"></i></button>
+           </div>`
+           lists.appendChild(element);
+           displayAlert("Add the item", "green")
+           todoContainer.classList.add("shows-container")
+        } else if (value && editFlag){
         console.log("editing")
     }else{
-        alerts.textContent = "Empty Value";
-        alerts.classList.add("alert-dangers")
+        displayAlert("please enter your value", "danger")
     }
 }
 
+function displayAlert( text, action){
+    alerts.textContent = text;
+    alerts.classList.add(`alert-${action}`)
 
+    setTimeout(function (){
+        alerts.textContent = "";
+        alerts.classList.remove(`alert-${action}`)
+    }
+    ,1000)
+}
 
