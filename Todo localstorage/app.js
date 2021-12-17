25b1cd39e3f7747b16ff51309f419f735fc83cc1
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-12-17 07:03:48 -0800

    diaplay the empty value

--- Todo localstorage/app.js
+++ Todo localstorage/app.js
@@ -1,12 +1,34 @@
-const form = document.querySelector(".from-control")
+const forms = document.querySelector(".from-control")
 const inputControl = document.getElementById("input");
 const todoContainer = document.querySelector(".todo-container")
 const lists = document.querySelector(".list-container")
 const clearBtn = document.querySelector(".clear-btn")
+const alerts = document.querySelector(".alert")
 
 // edit options
 
 let editElement ;
 let editFlag = false;
 let editID = "";
+
+// submit for
+forms.addEventListener("submit",addItem)
+function addItem(e){
+    e.preventDefault();
+    const value = inputControl.value;
+   
+  
+    const id = new Date().getTime().toString();
+   
+    if (value && !editFlag) {
+        console.log("add item to list")
+    } else if (value && editFlag){
+        console.log("editing")
+    }else{
+        alerts.textContent = "Empty Value";
+        alerts.classList.add("alert-dangers")
+    }
+}
+
+
 
