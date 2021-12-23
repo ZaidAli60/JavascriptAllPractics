23ef7fad75202a59e5f6f8f0efefedbc2f444200
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-12-23 08:25:33 -0800

    module

---
+++ Advance Javascript/Module/app.js
@@ -0,0 +1,16 @@
+import {message, user,human} from "./file.js"
+
+// console.log(message)
+
+console.log(user("Zaid ALi"))
+
+ let a = new human();
+
+//  shortcut way import the value
+// import * as zaid from "./file.js"
+
+// console.log(zaid.message)
+
+// console.log(zaid.user("Zaid ALi"))
+
+//  let a = new zaid.human();
---
+++ Advance Javascript/Module/file.js
@@ -0,0 +1,16 @@
+let message = "Zaid ALi";
+
+ function user(name){
+return ` ${name}`;
+}
+
+
+ class human {
+    constructor(){
+        console.log("consturctor Method")
+    }
+}
+
+// shortcut way export the value
+
+export {message,user,human}
