661fc6ccec58ffb1bd3de45486d437e1318b0d23
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-12-24 20:33:07 -0800

    settimeout

--- Advance Javascript/promise/app.js
+++ Advance Javascript/promise/app.js
@@ -10,30 +10,76 @@
 // console.log(promis);
 
 
-function promis(complete) {
-    // let complete = false;
- return  new Promise(function (resolve,reject) {
-    setTimeout(function name(params) {
-        if (complete) {
-            resolve("I am sccess");
-        }else{
-            reject("I am failure");
-        }
-    },3000)
-})
-}
+// function promis(complete) {
+//     // let complete = false;
+//  return  new Promise(function (resolve,reject) {
+//     setTimeout(function name(params) {
+//         if (complete) {
+//             resolve("I am sccess");
+//         }else{
+//             reject("I am failure");
+//         }
+//     },3000)
+// })
+// }
 
-let onfulfilment = (result) =>{
-    console.log(result)
-}
-let onRejected = (error) =>{
-    console.log(error)
-}
+// let onfulfilment = (result) =>{
+//     console.log(result)
+// }
+// let onRejected = (error) =>{
+//     console.log(error)
+// }
 
-promis(false).then(onfulfilment).catch(onRejected);
+// promis(false).then(onfulfilment).catch(onRejected);
 
 
 
 // console.log(promis(false));
 
+
+
+// function myFun(complete) {
+//     return new Promise(function (resolve,reject) {
+//      setTimeout(function name(params) {
+//         if (complete) {
+//             resolve("I am resolve");
+//         }else{
+//             reject("l am reject ");
+//         }
+//      },3000)
+//     })
+   
+// }
+// let onfulfilment = (result) =>{
+//     console.log(result);
+// }
+
+// let onRejected = (error) =>{
+//     console.log(error);
+// }
+
+// myFun(true).then(onfulfilment).catch(onRejected);
+// console.log(myFun(false))
+
+function myFun(complete) {
+    return new Promise(function (resolve,reject) {
+     setTimeout(function name(params) {
+        if (complete) {
+            resolve("I am resolve");
+        }else{
+            reject("l am reject ");
+        }
+     },3000)
+    })
+   
+}
+let onfulfilment = function (result) {
+    console.log(result);
+}
+
+let onRejected = function (error) {
+    console.log(error)
+}
+
+myFun(false).then(onfulfilment).catch(onRejected);
 
