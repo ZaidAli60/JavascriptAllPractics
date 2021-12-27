ac431ca4c7872d8d87a3689a871800cf00aeea39
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-12-26 22:39:30 -0800

    promise.all

--- Advance Javascript/promise/app.js
+++ Advance Javascript/promise/app.js
@@ -61,25 +61,88 @@
 // myFun(true).then(onfulfilment).catch(onRejected);
 // console.log(myFun(false))
 
-function myFun(complete) {
-    return new Promise(function (resolve,reject) {
-     setTimeout(function name(params) {
-        if (complete) {
-            resolve("I am resolve");
-        }else{
-            reject("l am reject ");
-        }
-     },3000)
-    })
+// function myFun(complete) {
+//     return new Promise(function (resolve,reject) {
+//      setTimeout(function name(params) {
+//         if (complete) {
+//             resolve("I am resolve");
+//         }else{
+//             reject("l am reject ");
+//         }
+//      },3000)
+//     })
    
-}
-let onfulfilment = function (result) {
-    console.log(result);
-}
+// }
+// let onfulfilment = function (result) {
+//     console.log(result);
+// }
 
-let onRejected = function (error) {
-    console.log(error)
-}
+// let onRejected = function (error) {
+//     console.log(error)
+// }
 
-myFun(false).then(onfulfilment).catch(onRejected);
+// myFun(false ).then(onfulfilment).catch(onRejected);
 
+
+
+          // Promise.all
+   
+ 
+
+//    let a = new Promise(function (resolve,reject){
+//     setTimeout(function (){
+//         console.log("The first value has resolve");    
+//     resolve(10);
+//     },1 * 1000)
+//    })       
+
+//    let b = new Promise(function (resolve,reject){
+//     setTimeout(function (){
+//         console.log("The secound value has resolve");    
+//     resolve(20);
+//     },2 * 1000)
+//    })   
+    
+//    let c = new Promise(function (resolve,reject){
+//     setTimeout(function (){
+//         console.log("The third value has resolve");    
+//     resolve(30);
+//     },3 * 1000)
+//    })   
+
+
+
+    let a = new Promise((resovle, reject) =>{
+        setTimeout(() => {
+            console.log("The first value has resolve")
+            resovle(10)
+        }, 1 * 1000);
+    });
+    let b = new Promise((resovle, reject) =>{
+        setTimeout(() => {
+            console.log("The secound value has rejected")
+            reject("Fail")
+        }, 2 * 1000);
+    });
+
+    let c = new Promise((resovle, reject) =>{
+        setTimeout(() => {
+            console.log("The third value has resolve")
+            resovle(30)
+        }, 3 * 1000);
+    });
+
+    var total = 0;
+   Promise.all([a,b,c]).then((result) =>{
+  for(var i in result){
+      total += result[i]
+
+  }
+  console.log(`Result : ${result}`)
+  console.log(`Total : ${total}`)
+
+
+}).catch((error) =>{
+    console.log(`error : ${error}`)
+})
+    
