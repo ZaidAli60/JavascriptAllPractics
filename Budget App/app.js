057078d8b2a09ffb52fc8ba9e92695d71719d44f
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2022-01-02 01:06:41 -0800

    show balance

--- Budget App/app.js
+++ Budget App/app.js
@@ -3,6 +3,9 @@ class Budget {
         this.feedbackAlert = document.querySelector(".aalert")
         this.budgetInput = document.getElementById("budget-input")
         this.budgetAmount = document.getElementById("budget-amount")
+        this.budgetExpense = document.getElementById("expanes-amount")
+        this.budgetBalance = document.getElementById("balance-amount")
+        this.balance = document.getElementById("balance")
     }
     //budget form method
     submitFormBudget(){
@@ -20,10 +23,22 @@ class Budget {
             this.budgetAmount.textContent = value;
             // console.log(this.budgetAmount)
             this.budgetInput.value = "";
+            this.showBalance();
         }
-      
-
     }
+    // show the balance
+    showBalance(){
+        const expense = this.totalExpense();
+        const total = parseInt(this.budgetAmount.textContent) - expense;
+        this.budgetBalance.textContent = total;
+        // console.log(total)
+    }
+    totalExpense(){
+        let total = 0;
+        return total;
+    }
+
+
 
 }
 
